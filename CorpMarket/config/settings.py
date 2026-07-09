c976662f2f758d4e650a1054bb83ed7cfee70f18
ebobr <eg.lyamin@yandex.ru>
2026-07-09 22:55:35 +0300

    Добавил apps в INSTALLED_APPS убрал .idea с .gitignore

--- CorpMarket/config/settings.py
+++ CorpMarket/config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'users.apps.UsersConfig',
+    'adverts.apps.AdvertsConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
