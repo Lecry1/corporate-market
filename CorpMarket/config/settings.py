25e4cf22c3e334cab231c887f9d42bc17ec9a194
Саша <dddaddsnipher132@gmail.com>
2026-07-13 00:18:10 +0300

    integrate photo and chat models

--- CorpMarket/config/settings.py
+++ CorpMarket/config/settings.py
@@ -10,12 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-import os
-
 from dotenv import load_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'users.apps.UsersConfig',
     'adverts.apps.AdvertsConfig',
+    'photos.apps.PhotosConfig',
+    'chats.apps.ChatsConfig',
     'reviews.apps.ReviewsConfig',
 ]
 
