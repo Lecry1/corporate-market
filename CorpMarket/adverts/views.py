87d0b1b70a9caa090d2a660d911e00d0de4c9360
Саша <dddaddsnipher132@gmail.com>
2026-07-13 00:22:19 +0300

    secure advert editing

--- CorpMarket/adverts/views.py
+++ CorpMarket/adverts/views.py
@@ -1,3 +1,5 @@
+from adverts.models import Advert
+from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -8,31 +10,41 @@ from django.views.generic import (
     UpdateView,
 )
 
-from .models import Advert
+
+class BootstrapFormMixin:
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+
+        for field in form.fields.values():
+            css_class = 'form-select' if isinstance(field.widget, forms.Select) else 'form-control'
+            field.widget.attrs['class'] = css_class
+
+        return form
 
 
 class AdvertListView(ListView):
     model = Advert
-    template_name = "adverts/advert_list.html"
-    context_object_name = "adverts"
+    template_name = 'adverts/advert_list.html'
+    context_object_name = 'adverts'
     paginate_by = 6
 
 
 class AdvertDetailView(DetailView):
     model = Advert
-    template_name = "adverts/advert_detail.html"
-    context_object_name = "advert"
+    template_name = 'adverts/advert_detail.html'
+    context_object_name = 'advert'
 
 
-class AdvertCreateView(LoginRequiredMixin, CreateView):
+class AdvertCreateView(BootstrapFormMixin, LoginRequiredMixin, CreateView):
     model = Advert
-    template_name = "adverts/advert_form.html"
+    template_name = 'adverts/advert_form.html'
     fields = (
-        "title",
-        "description",
-        "price",
-        "category",
-        "address",
+        'title',
+        'description',
+        'price',
+        'category',
+        'address',
     )
 
     def form_valid(self, form):
@@ -41,27 +53,25 @@ class AdvertCreateView(LoginRequiredMixin, CreateView):
 
 
 class AdvertUpdateView(
+    BootstrapFormMixin,
     LoginRequiredMixin,
     UserPassesTestMixin,
     UpdateView,
 ):
     model = Advert
-    template_name = "adverts/advert_form.html"
+    template_name = 'adverts/advert_form.html'
     fields = (
-        "title",
-        "description",
-        "price",
-        "category",
-        "status",
-        "address",
+        'title',
+        'description',
+        'price',
+        'category',
+        'status',
+        'address',
     )
 
     def test_func(self):
         advert = self.get_object()
-        return (
-            advert.seller == self.request.user
-            or self.request.user.is_superuser
-        )
+        return advert.seller == self.request.user or self.request.user.is_market_admin
 
 
 class AdvertDeleteView(
@@ -70,12 +80,9 @@ class AdvertDeleteView(
     DeleteView,
 ):
     model = Advert
-    template_name = "adverts/advert_confirm_delete.html"
-    success_url = reverse_lazy("adverts:list")
+    template_name = 'adverts/advert_confirm_delete.html'
+    success_url = reverse_lazy('adverts:list')
 
     def test_func(self):
         advert = self.get_object()
-        return (
-            advert.seller == self.request.user
-            or self.request.user.is_superuser
-        )
+        return advert.seller == self.request.user or self.request.user.is_market_admin
