c9fd872e76b259f2bcd1ff2cd9c8b24785af5b81
ebobr <eg.lyamin@yandex.ru>
2026-07-12 17:20:42 +0300

    CRUD объявления

--- CorpMarket/adverts/views.py
+++ CorpMarket/adverts/views.py
@@ -1,3 +1,75 @@
-# from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
-# Create your views here.
+from .models import Advert
+
+
+class AdvertListView(ListView):
+    model = Advert
+    template_name = "adverts/advert_list.html"
+    context_object_name = "adverts"
+    paginate_by = 6
+
+
+class AdvertDetailView(DetailView):
+    model = Advert
+    template_name = "adverts/advert_detail.html"
+    context_object_name = "advert"
+
+
+class AdvertCreateView(LoginRequiredMixin, CreateView):
+    model = Advert
+    template_name = "adverts/advert_form.html"
+    fields = (
+        "title",
+        "description",
+        "price",
+        "category",
+        "address",
+    )
+
+    def form_valid(self, form):
+        form.instance.seller = self.request.user
+        return super().form_valid(form)
+
+
+class AdvertUpdateView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    UpdateView,
+):
+    model = Advert
+    template_name = "adverts/advert_form.html"
+    fields = (
+        "title",
+        "description",
+        "price",
+        "category",
+        "status",
+        "address",
+    )
+
+    def test_func(self):
+        advert = self.get_object()
+        return advert.seller == self.request.user
+
+
+class AdvertDeleteView(
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+    DeleteView,
+):
+    model = Advert
+    template_name = "adverts/advert_confirm_delete.html"
+    success_url = reverse_lazy("adverts:list")
+
+    def test_func(self):
+        advert = self.get_object()
+        return advert.seller == self.request.user
