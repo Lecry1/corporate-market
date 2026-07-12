9671ed7e9655202769824feea0a7fa00beba2404
ebobr <66569467+ebobr@users.noreply.github.com>
2026-07-12 19:40:39 +0300

    Feature/crud adverts (#3)
    * Настройка окружения разработчика (#1)
    
    * Добавлена модель 'review' для базы данных.
    
    * Инициализация Django project
    
    * Readme стал LF, добавил django в зависимости
    
    * добавил .idea в gitignore
    
    * Добавлена модель User
    Добавлен пропущенный manage.py
    удалён models/review.py от freshburik
    
    * Добавлена модель Advert + её методы
    PEP 8 fix settings.py
    
    * Добавлена модель Review + некоторые её методы
    
    * разделение requirements на requirements.txt и requirements-dev.txt
    
    * Добавил Pillow в зависимости
    Убрал default_auto_field из моделей (т.к. перешли на django 6)
    
    * Добавил apps в INSTALLED_APPS
    убрал .idea с .gitignore
    
    * Добавлены базовые обращения к базе данных Reviews
    
    * Перевёл на PostgreSQL, см. обновлённый README.md
    
    * Зарегистрировал модели в Админке
    
    * Реализация Бд и Django в докерКонтейнеры
    
    * model photo was added
    
    * model chats was added
    
    * Обновил README.md
    Добавил .dockerignore
    убрал версию из docker-compose.yml
    
    * CRUD объявления
    
    * минимальные html под CRUD объявления
    
    * скорректировал TIME_ZONE
    
    * Дал Админу возможность редактировать и удалять ВСЕ объявления
    
    ---------
    
    Co-authored-by: _Lecry_ <103503184+Lecry1@users.noreply.github.com>
    Co-authored-by: Соколов Сергей Константинович <ssk24u719@student.bmstu.ru>
    Co-authored-by: Vlad Glebov <glebov.vs.vs@gmail.com>
    Co-authored-by: Rushbro <dashkinrr@student.bmstu.ru>

---
+++ CorpMarket/config/settings.py
@@ -0,0 +1,119 @@
+from pathlib import Path
+
+import os
+
+from dotenv import load_dotenv
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(dotenv_path=BASE_DIR.parent / '.env')
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-fallback-secret-key-for-dev')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    'users.apps.UsersConfig',
+    'adverts.apps.AdvertsConfig',
+    'reviews.apps.ReviewsConfig',
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "config.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'corporate_market'),
+        'USER': os.environ.get('DB_USER', 'corpmarket_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'your_password'),
+        # Для Docker-контейнеров здесь должно быть имя сервиса БД из docker-compose, т.е. 'db'
+        'HOST': os.environ.get('DB_HOST', 'db'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/6.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Europe/Moscow"
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
+STATIC_URL = "static/"
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+LOGIN_REDIRECT_URL = "adverts:list"
+LOGOUT_REDIRECT_URL = "adverts:list"
