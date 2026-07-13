9f6f4b171f4bc89d2badacefac1097bfe5008991
fresh_burikh <falconser11@gmail.com>
2026-07-13 03:47:09 +0300

    Добавлен базовый класс и фикстуры для тестирования авторизации

--- CorpMarket/users/tests.py
+++ CorpMarket/users/tests.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+User = get_user_model()
+
+
+# Базовый класс с общими методами для тестов
+class BaseUserTest(TestCase):
+
+    def setUp(self):
+        self.register_url = reverse('users:register')
+        self.login_url = reverse('users:login')
+        self.logout_url = reverse('users:logout')
+        self.adverts_list_url = reverse('adverts:list')
+
+    def create_test_user(self, username='testuser', password='StrongPass123!'):
+        return User.objects.create_user(username=username, email='test@example.com', password=password)
