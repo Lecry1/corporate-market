3c0020ac816ce33283851f189b0fd12dab41fd3f
fresh_burikh <falconser11@gmail.com>
2026-07-13 03:50:25 +0300

    Добавлены тесты доступности страниц

--- CorpMarket/users/tests.py
+++ CorpMarket/users/tests.py
@@ -16,3 +16,24 @@ class BaseUserTest(TestCase):
 
     def create_test_user(self, username='testuser', password='StrongPass123!'):
         return User.objects.create_user(username=username, email='test@example.com', password=password)
+
+
+# Тесты доступности страниц аутентификации
+class AuthPageTest(BaseUserTest):
+
+    # Страница регистрации должна быть доступна
+    def test_register_page_accessible(self):
+        response = self.client.get(self.register_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    # Страница входа должна быть доступна
+    def test_login_page_accessible(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/login.html')
+
+    # Выход должен требовать POST запрос
+    def test_logout_requires_post(self):
+        response = self.client.get(self.logout_url)
+        self.assertEqual(response.status_code, 405)
